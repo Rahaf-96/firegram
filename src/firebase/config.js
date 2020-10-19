2c8a5d42f0477b807a813683ae30bb1b231cde19
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-10-19 23:35:32 +0300

    initial files

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyCofODsQw1nWGo79yOP16l_P2_RujBFrho',
+  authDomain: 'firegram-cb001.firebaseapp.com',
+  databaseURL: 'https://firegram-cb001.firebaseio.com',
+  projectId: 'firegram-cb001',
+  storageBucket: 'firegram-cb001.appspot.com',
+  messagingSenderId: '825506474476',
+  appId: '1:825506474476:web:dc1692d57876564d57b8c9',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
